c257c4c38d6a314218011f9e69815bc47c9837b9
akihito7 <akihitopro7@gmail.com>
2025-08-15 15:56:54 -0300

    fix : change api to use env

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import { getCookies } from "@/actions/cookies";
 import axios from "axios"
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3030',
+  baseURL: process.env.NEXT_PUBLIC_APIURL,
 });
 
 api.interceptors.request.use(async (config) => {
