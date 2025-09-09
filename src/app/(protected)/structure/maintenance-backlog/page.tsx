4947e65f977d5333ea97305101a5070e54759e88
akihito7 <akihitopro7@gmail.com>
2025-09-09 09:21:08 -0300

    feat: add overflow-x-hidden class to main for improved layout handling

--- src/app/(protected)/structure/maintenance-backlog/page.tsx
+++ src/app/(protected)/structure/maintenance-backlog/page.tsx
@@ -93,7 +93,7 @@ export default function MaintenanceBacklog() {
   });
 
   return (
-    <main className="bg-gray-50 min-h-screen w-full p-0 py-8 px-2 sm:p-8 flex flex-col gap-6">
+    <main className="bg-gray-50 overflow-x-hidden min-h-screen w-full p-0 py-8 px-2 sm:p-8 flex flex-col gap-6">
       <div className="space-y-2">
         <div className="flex items-center mb-8 gap-2">
           <ButtonOpenSidebar />
