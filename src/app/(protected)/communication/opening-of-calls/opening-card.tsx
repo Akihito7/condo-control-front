d3eb4054025f13b766251b547064be56d1be87f9
akihito7 <akihitopro7@gmail.com>
2025-09-13 22:09:36 -0300

    feat: add time format toggle to OpeningCard component for improved hour display

--- src/app/(protected)/communication/opening-of-calls/opening-card.tsx
+++ src/app/(protected)/communication/opening-of-calls/opening-card.tsx
@@ -1,5 +1,7 @@
+import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import React from "react";
+import { Clock } from "lucide-react";
+import React, { useState } from "react";
 
 interface OpeningCard {
   title: string;
@@ -14,11 +16,30 @@ export function OpeningCard({
   value,
   isHours = false,
 }: OpeningCard) {
+  const [formatHoursType, setFormatHoursType] = useState<"normal" | "00:00">(
+    "normal"
+  );
+
   const formatHours = (hoursDecimal: number) => {
     const hours = Math.floor(hoursDecimal);
     const minutes = Math.round((hoursDecimal - hours) * 60);
-    return `${hours}h ${minutes}min`;
+
+    if (formatHoursType === "normal") {
+      return `${hours}h ${minutes}min`;
+    } else {
+      const hh = String(hours).padStart(2, "0");
+      const mm = String(minutes).padStart(2, "0");
+      return `${hh}:${mm}`;
+    }
   };
+
+  function toggleTypeHours() {
+    if (formatHoursType === "normal") {
+      return setFormatHoursType("00:00");
+    }
+    setFormatHoursType("normal");
+  }
+
   return (
     <Card className="min-w-[210px] max-w-[350px]cursor-pointer flex justify-between">
       <CardHeader className="flex items-center justify-between pb-2">
@@ -31,7 +52,17 @@ export function OpeningCard({
       </CardHeader>
       <CardContent className="flex flex-col gap-1">
         <span className={`text-2xl md:text-3xl font-bold dark:text-foreground`}>
-          {isHours ? formatHours(value) : value.toFixed(0)}
+          {isHours ? formatHours(value) : value.toFixed(0)}{" "}
+          {isHours && (
+            <Button
+              onClick={toggleTypeHours}
+              size="icon"
+              variant="ghost"
+              className="h-8 w-8 cursor-pointer"
+            >
+              <Clock className="h-5 w-5" />
+            </Button>
+          )}
         </span>
       </CardContent>
     </Card>
