2419f06773a2bc8cb133f33ba8bd16763467d829
akihitoo7 <akihitopro7@gmail.com>
2025-11-29 17:19:45 -0300

    fix: adjusted tanstack to dont refetch every time user exit and back to screen

--- src/providers/query-provider.tsx
+++ src/providers/query-provider.tsx
@@ -4,7 +4,18 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React, { useState } from "react";
 
 export function QueryProvider({ children }: { children: React.ReactNode }) {
-  const [clientQuery] = useState(() => new QueryClient());
+  const [clientQuery] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            refetchOnWindowFocus: false,
+            refetchOnReconnect: false,
+            refetchOnMount: false,
+          },
+        },
+      })
+  );
   return (
     <QueryClientProvider client={clientQuery}>{children}</QueryClientProvider>
   );
